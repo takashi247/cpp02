3cfeb2dc36fbc36ecdc867c1b41c3df53dab039d
takashi247 <takashi247@gmail.com>
2022-01-22 22:20:10 +0900

    add error handling for overflow in ex03 and update test cases

--- ex03/srcs/bsp.cpp
+++ ex03/srcs/bsp.cpp
@@ -1,5 +1,9 @@
 #include "Point.hpp"
 
+/*
+** Implementation of bsp by utilizing areas of triangles
+*/
+
 bool
 	isInside(Fixed abc, Fixed abp, Fixed bcp, Fixed cap)
 {
@@ -21,25 +25,6 @@ Fixed
 	return (area / Fixed(2));
 }
 
-// bool
-// 	isInside(Fixed &ap, Fixed &bp, Fixed &cp)
-// {
-// 	if (ap > 0.0f && bp > 0.0f && cp > 0.0f)
-// 		return (true);
-// 	else if (ap < 0.0f && bp < 0.0f && cp < 0.0f)
-// 		return (true);
-// 	else
-// 		return (false);
-// }
-
-// Fixed
-// 	computeCrossProd(Point const &a, Point const &b, Point const &point)
-// {
-// 	Point	v1 = b - a;
-// 	Point	v2 = point - a;
-// 	return (v1.getX() * v2.getY() - v1.getY() * v2.getX());
-// }
-
 bool
 	bsp(Point const a, Point const b, Point const c, Point const point)
 {
@@ -53,11 +38,41 @@ bool
 	std::cout << "area of tri_cap: " << tri_cap << std::endl;
 	std::cout << "tri_abp + tri_bcp + tri_cap = " << tri_abp + tri_bcp + tri_cap << std::endl;
 	return (isInside(tri_abc, tri_abp, tri_bcp, tri_cap));
-	// Fixed	cross_prod_ap = computeCrossProd(a, b, point);
-	// Fixed	cross_prod_bp = computeCrossProd(b, c, point);
-	// Fixed	cross_prod_cp = computeCrossProd(c, a, point);
-	// std::cout << "cross product of a and p: " << cross_prod_ap << std::endl;
-	// std::cout << "cross product of b and p: " << cross_prod_bp << std::endl;
-	// std::cout << "cross product of c and p: " << cross_prod_cp << std::endl;
-	// return (isInside(cross_prod_ap, cross_prod_bp, cross_prod_cp));
 }
+
+/*
+** Implementation of bsp by utilizing cross products
+*/
+
+/*
+bool
+	isInside(Fixed &ap, Fixed &bp, Fixed &cp)
+{
+	if (ap > 0.0f && bp > 0.0f && cp > 0.0f)
+		return (true);
+	else if (ap < 0.0f && bp < 0.0f && cp < 0.0f)
+		return (true);
+	else
+		return (false);
+}
+
+Fixed
+	computeCrossProd(Point const &a, Point const &b, Point const &point)
+{
+	Point	v1 = b - a;
+	Point	v2 = point - a;
+	return (v1.getX() * v2.getY() - v1.getY() * v2.getX());
+}
+
+bool
+	bsp(Point const a, Point const b, Point const c, Point const point)
+{
+	Fixed	cross_prod_ap = computeCrossProd(a, b, point);
+	Fixed	cross_prod_bp = computeCrossProd(b, c, point);
+	Fixed	cross_prod_cp = computeCrossProd(c, a, point);
+	std::cout << "cross product of a and p: " << cross_prod_ap << std::endl;
+	std::cout << "cross product of b and p: " << cross_prod_bp << std::endl;
+	std::cout << "cross product of c and p: " << cross_prod_cp << std::endl;
+	return (isInside(cross_prod_ap, cross_prod_bp, cross_prod_cp));
+}
+*/
